4c9a86f1ec31f48f162e36c8f75603447e061b53
Matan Kushner <hello@matchai.dev>
2024-11-11 14:53:59 +0900

    Have dialog dismiss upon login

--- satori_reader_login.py
+++ satori_reader_login.py
@@ -2,13 +2,13 @@ from aqt import AnkiQt
 from aqt.qt import *
 
 def display_login_dialog(mw: AnkiQt):
+  # Create and configure the dialog
   d = QDialog()
   d.setWindowTitle("Satori to Anki")
   d.setWindowModality(Qt.WindowModality.WindowModal)
-  d.resize(600, 650)
+  d.resize(600, 660)
 
   url = "https://www.satorireader.com/signin"
-
   token = None
 
   def on_cookie_added(cookie) -> None:
@@ -17,24 +17,28 @@ def display_login_dialog(mw: AnkiQt):
     if cookie.name() != b"SessionToken":
       return
 
+    # Close the dialog when the token is received
     token = cookie.value()
+    d.accept()
 
+  # Setup a profile to capture cookies
   webview = QWebEngineView()
   profile = QWebEngineProfile("storage", webview)
   cookie_store = profile.cookieStore()
   cookie_store.cookieAdded.connect(on_cookie_added)
 
+  # Load the login URL
   webpage = QWebEnginePage(profile, webview)
   webview.setPage(webpage)
   webview.load(QUrl(url))
 
+  # Add the webview to a dialog layout
   layout = QVBoxLayout()
   layout.addWidget(webview)
   d.setLayout(layout)
 
-  d.show()
+  # Show the dialog until it completes
   d.exec()
-
   webview.destroy()
 
   return token
